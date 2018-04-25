e3f9a38952077f8b2d9044dc883121d9cfdee66c
BertrandBurel <legionxful@gmail.com>
2018-04-25 14:32:14 +0200

    TDD part 1

--- src/main/object-literals.spec.ts
+++ src/main/object-literals.spec.ts
@@ -6,12 +6,11 @@ describe('enhanced object literals', () => {
       // Using NEW Object Literal Syntax, return a literal that will allow the tests to pass
       return {
         type: 'Musician',
-        // - in ES5 you'd do:
-        // name: name,
-        // dead: dead,
-        // wantsToPlayWith: function (target){
-        //   return `${this.name} wants to play with ${target.name} ${target.dead ? 'but he is' : 'and he is not'} dead`;
-        // }
+        name: name,
+        dead: dead,
+        wantsToPlayWith: function (target){
+          return `${this.name} wants to play with ${target.name} ${target.dead ? 'but he is' : 'and he is not'} dead`;
+        }
         // - do that in ES6 !
       }
     }
